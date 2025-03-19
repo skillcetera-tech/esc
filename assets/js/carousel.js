41e4cefe48320c96e793aa0819ea09cfb5e938fd
dduyanhhoang <dduyanhhoang@gmail.com>
2025-03-20 04:55:45 +0700

    feat(index): add script to make carousel run infinitely

---
+++ assets/js/carousel.js
@@ -0,0 +1,7 @@
+var logosSlide = document.querySelector(".logos-slide");
+var logosClone = logosSlide.cloneNode(true);
+document.querySelector(".logos").appendChild(logosClone);
+
+var namesSlide = document.querySelector(".names-slide");
+var namesClone = namesSlide.cloneNode(true);
+document.querySelector(".names").appendChild(namesClone);
